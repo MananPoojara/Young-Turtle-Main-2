c720ca1c1ea686e77644c60f142664ee036deeb9
DharmilVirani <dharmilvirani@gmail.com>
2025-11-27 11:05:17 +0530

    init commit

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,16 +6,6 @@ const HeroSection = () => {
         <section className="relative w-full h-screen flex items-center justify-center overflow-hidden">
             {/* Video Background */}
             <video
-<<<<<<< HEAD
-            autoPlay
-            muted
-            loop
-            playsInline
-            preload="metadata"
-            className="absolute inset-0 w-full h-full object-contain scale-100"
-            >
-                <source src="/turtle.mp4" type="video/mp4" />
-=======
                 autoPlay
                 muted
                 loop
@@ -25,7 +15,6 @@ const HeroSection = () => {
             >
                 <source src="/turtle2.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
->>>>>>> c2d693e3db0aa55a08fc1a8a08f737d9c2c4de7d
             </video>
 
 
@@ -34,9 +23,6 @@ const HeroSection = () => {
 
             {/* Content */}
             <div className="relative z-10 text-center text-white px-4 sm:px-6 lg:px-8 max-w-4xl">
-<<<<<<< HEAD
-                <BlurTextAnimation text="We don’t predict the markets. We measure them" className="bg-transparent" />
-=======
                 <BlurTextAnimation className="bg-transparent " />
                 {/* Buttons */}
                 {/* <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
@@ -48,7 +34,6 @@ const HeroSection = () => {
                         Learn More
                     </button>
                 </div> */}
->>>>>>> c2d693e3db0aa55a08fc1a8a08f737d9c2c4de7d
             </div>
         </section>
     )
