ce7957c1fb114ab3d8a7077da7488e21bec84850
DharmilVirani <dharmilvirani@gmail.com>
2025-11-28 11:39:19 +0530

    dharmil

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,34 +3,26 @@ import BlurTextAnimation from './ui/blur-text-animation'
 
 const HeroSection = () => {
     return (
-        <section className="relative w-full  h-screen flex items-center justify-center overflow-hidden">
+        <section className='relative w-full  h-screen flex items-center justify-center overflow-hidden'>
             {/* Video Background */}
             <video
                 autoPlay
                 muted
                 loop
                 playsInline
-                preload="metadata"
-                className="absolute inset-0 w-full h-full object-cover"
+                preload='metadata'
+                className='absolute inset-0 w-full h-full object-cover'
             >
-                <source src="/turtle2_crop.mp4" type="video/mp4" />
+                <source src='/turtle2_crop.mp4' type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
 
-
             {/* Overlay */}
-<<<<<<< HEAD
-            <div className="absolute inset-0 bg-black/20"></div>
+            <div className='absolute inset-0 bg-[#275669]/20 lg:bg-[#275669]/20'></div>
 
             {/* Content */}
-            <div className="relative z-10 text-center text-white px-4 sm:px-6 md:px-8 lg:px- w-[210%]">
-=======
-            <div className="absolute inset-0 bg-[#275669]/20 lg:bg-[#275669]/20"></div>
-
-            {/* Content */}
-            <div className="relative z-10 text-center text-white px-4 sm:px-4 md:px-4 lg:px-12 w-[110%]">
->>>>>>> c990bd33c578e7d30dc86459ce71e11824d52976
-                <BlurTextAnimation className="bg-transparent" />
+            <div className='relative z-10 text-center text-white px-4 sm:px-4 md:px-4 lg:px-12 w-[110%]'>
+                <BlurTextAnimation className='bg-transparent' />
                 {/* Buttons */}
                 {/* <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
                     <button className="px-8 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300 transform hover:scale-105">
