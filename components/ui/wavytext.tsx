bcb2247f0f5c5fb645eeb2bd2965aeb78d692fd5
DharmilVirani <dharmilvirani@gmail.com>
2025-12-04 15:12:56 +0530

    init commit

--- components/ui/wavytext.tsx
+++ components/ui/wavytext.tsx
@@ -1,23 +1,24 @@
 export const TortoiseQuote = () => {
     return (
-        <div className="text-left md:text-left px-4 md:px-0 max-w-7xl">
+        <div className='text-left md:text-left px-4 md:px-0 max-w-7xl'>
             <h1
-                className="
+                className='
                     font-serif 
                     text-white 
-                    text-4xl 2xl:text-5xl md:text-5xl lg:text-6xl 
+                    sm:text-5xl md:text-5xl lg:text-6xl xl:text-5xl 2xl:text-5xl
                     leading-tight 
                     tracking-tight
-                "
+                '
             >
                 The future of wealth is quant-driven and human-centered.
                 <br />
-                Bridging ultra-modern <span className="text-[#ffb900] italic">research with grounded financial wisdom.</span>{" "}
+                Bridging ultra-modern{' '}
+                <span className='text-[#ffb900] italic'>research with grounded financial wisdom.</span>{' '}
             </h1>
 
-            <p className="mt-3 text-white/80 text-lg md:text-xl font-light tracking-wide">
+            <p className='mt-3 text-white/80 text-lg md:text-xl font-light tracking-wide'>
                 Expectations beat predictions
             </p>
         </div>
-    );
-};
+    )
+}
